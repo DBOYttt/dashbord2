fee55ffb3c1a8e3bc6582fa7d0dc43d90643259d
D-BOY <65898012+DBOYttt@users.noreply.github.com>
2022-11-09 11:54:34 +0100

    separate common operations from main file to special file for that

---
+++ common_array_operations.php
@@ -0,0 +1,30 @@
+<?php
+    $common = count($my_arr);
+
+    // for($i = $common; $i > 0 ; $i--){
+        
+        for($x = 0; $x <= ($common - 1) ; $x++) {
+
+        // echo $x . "<br>";
+        // echo $common . "<br>";
+
+        $dbname = $my_arr[$x];
+        $dbname_for_mysql = $dbname;
+
+        $link = new mysqli($servername, $username, $password, $dbname);
+        if ($link->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+          }
+
+
+        $db_list = mysqli_query($link, "SELECT table_name FROM information_schema.tables WHERE table_schema = '$dbname_for_mysql'");
+
+        if ($db_list->num_rows > 0) {
+            while($row = $db_list->fetch_assoc()) {
+                 echo $row['table_name'] . '<br>';
+            }
+        }
+        echo "<br>";
+        unset($my_arr[$x]);
+    }
+?>
--- main.php
+++ main.php
@@ -30,35 +30,7 @@ echo "<div class = 'group1'>";
        array_push($my_arr, $os);
     }
 echo "</div>";
-
-    $common = count($my_arr);
-
-    // for($i = $common; $i > 0 ; $i--){
-        
-        for($x = 0; $x <= ($common - 1) ; $x++) {
-
-        // echo $x . "<br>";
-        // echo $common . "<br>";
-
-        $dbname = $my_arr[$x];
-        $dbname_for_mysql = $dbname;
-
-        $link = new mysqli($servername, $username, $password, $dbname);
-        if ($link->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-          }
-
-
-        $db_list = mysqli_query($link, "SELECT table_name FROM information_schema.tables WHERE table_schema = '$dbname_for_mysql'");
-
-        if ($db_list->num_rows > 0) {
-            while($row = $db_list->fetch_assoc()) {
-                 echo $row['table_name'] . '<br>';
-            }
-        }
-        unset($my_arr[$x]);
-    }
-// }
+    include 'common_array_operations.php';  
 
     // if (in_array("w3schools", $my_arr)) {
     //     echo '<a href="index2.html"><button class="button3" href = "index.html">w3shools</button></a>';
